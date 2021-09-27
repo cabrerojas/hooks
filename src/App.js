e9963a4038eaef9c5d7eb0adcf138571b7551972
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-27 15:23:56 -0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
-import { useReducer, useState } from "react";
+import { useRef } from "react";
 
-// state =  { contador: 0 }
-// action  = { type: string, payload: any }
-
-
-const inicial = { contador: 0 }
-
-const reducer = (state, action) => {
-
-    switch (action.type) {
-        case 'incrementar':
-            return { contador: state.contador + 1 }
-        case 'decrementar':
-            return { contador: state.contador - 1 }
-        case 'set':
-            return { contador: action.payload }
-        default:
-            return state
-    }
-
-}
 
 const App = () => {
 
-    const [state, dispatch] = useReducer(reducer, inicial)
-    const [valor, setValor] = useState(0);
+    const ref = useRef();
+    const inputRef = useRef();
+    const click = () => {
+        console.log(ref.current.clientHeight);
+        ref.current.innerHTML = 'chanchito feliz'
+    }
+    const focus = () => {
+        inputRef.current.focus()
+    }
 
+  return (
 
-    return (
-
-        <div>
-            Contador: {state.contador}
-            <input value={valor} onChange={e => setValor(e.target.value)} />
-            <button onClick={() => dispatch({ type: 'incrementar' })} >Más</button>
-            <button onClick={() => dispatch({ type: 'decrementar' })} >Menos</button>
-            <button onClick={() => dispatch({ type: 'set', payload: valor })} >Set</button>
-        </div>
-    )
+    <div>
+        <input ref={inputRef} />
+    
+        <button onClick={focus} >Focus</button>
+        <div ref={ref} onClick={click} > Lala </div>
+    </div>
+  )
 
 }
 
